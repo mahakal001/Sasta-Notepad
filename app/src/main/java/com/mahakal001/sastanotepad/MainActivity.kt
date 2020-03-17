5d0540410aec31afc6a17ff08c111455ec458478
mkal001 <mohit.pilkhan.77@gmail.com>
2020-03-18 03:08:17 +0530

    Version 0.1 This commit introduce a floating action button on the Main activity. This fab button will be used for taking notes in the upcoming version.

---
+++ app/src/main/java/com/mahakal001/sastanotepad/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mahakal001.sastanotepad
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.mahakal001.sastanotepad.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+
+    }
+}
